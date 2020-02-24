0d2ef2188f87546d4bb55c0e6c2301f8adf60ece
Thidasala <thidasala.demintha@gmail.com>
2020-02-24 16:59:43 +0530

    Changed widget title

--- generator-widgetgenerator/app/index.js
+++ generator-widgetgenerator/app/index.js
@@ -25,7 +25,7 @@ module.exports = class extends Generator {
       {
         type: "input",
         name: "widgetHeading",
-        message: "Enter Widget Heading",
+        message: "Enter Widget Title",
         default: "Recent Api Traffic"
       },
       {
